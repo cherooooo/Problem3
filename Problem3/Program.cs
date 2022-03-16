0d993ac6cb1c0963987222ebf70f5701f8796b3b
cherooooo <101722878+cherooooo@users.noreply.github.com>
2022-03-16 21:39:08 +0800

    Add files via upload
    Initial commit

---
+++ Problem3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Date and Time Now:" + DateTime.Now);
+            Console.WriteLine("Please input a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Date and Time after " + number + " hours: " + DateTime.Now.AddHours(number));
+        }
+    }
+}
